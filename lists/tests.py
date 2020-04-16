560419a81fa534336a104b7d3dce6fd2c52ba55e
dima <dimich3d@ya.ru>
2020-04-16 22:22:14 +0300

    add first dummy view for home page and first passing unittest

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    def test_soso(self):
-        self.assertEqual(1 + 1, 4)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page(self):
+        self.assertEqual(resolve("/").func, home_page)
