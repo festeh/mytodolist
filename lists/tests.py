cfa6d5a03315176af9d8307850486f91f9f7925a
dima <dimich3d@ya.ru>
2020-04-17 20:01:39 +0300

    add home template and refactor unitest to use it

--- lists/tests.py
+++ lists/tests.py
@@ -5,13 +5,6 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        self.assertEqual(resolve("/").func, home_page)
-
     def test_home_page_resturns_correct_response(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html_content = response.content.decode("utf-8")
-        self.assertTrue(html_content.startswith("<html>"))
-        self.assertIn("<title>Todo list</title>", html_content)
-        self.assertTrue(html_content.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<html><title>Todo list</title></html>")
+    return render(request, "home.html")
