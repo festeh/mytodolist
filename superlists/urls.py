aa3bdaf0e21c0dd3d91675fd7194edd2851e3c92
dima <dimich3d@ya.ru>
2020-04-18 18:29:44 +0300

    refactor urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from lists.views import home_page, view_list, new_list, add_task
+from django.conf.urls import url, include
+from lists.views import home_page
+from lists import urls as list_urls
 
 urlpatterns = [
     url(r"^$", home_page, name="home"),
-    url(r"^lists/new$", new_list, name="new_list"),
-    url(r"^lists/(\d+)/$", view_list, name="view_list"),
-    url(r"^lists/(\d+)/add_task", add_task, name="add_task")
+    url(r"^lists/", include(list_urls)),
 ]
