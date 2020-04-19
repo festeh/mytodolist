4d0f09e688b7b48b5523907c93bbf52789c16b23
dima <dimich3d@ya.ru>
2020-04-19 16:25:17 +0300

    refactor unittests separate file

---
+++ lists/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from lists.models import List, Task
+
+
+class ListAndTaskModelTest(TestCase):
+    def test_save_load_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Task()
+        first_item.text = "First one"
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Task()
+        second_item.text = "The second"
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(list_, saved_list)
+
+        saved_items = Task.objects.all()
+        self.assertEqual(len(saved_items), 2)
+
+        self.assertEqual(saved_items[0].text, "First one")
+        self.assertEqual(saved_items[0].list, list_)
+        self.assertEqual(saved_items[1].text, "The second")
+        self.assertEqual(saved_items[1].list, list_)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from lists.models import List, Task
 
 
@@ -71,30 +72,3 @@ class NewTaskExistingListTest(TestCase):
         response = self.client.post(f"/lists/{our_list.id}/add_task",
                                     data={"task_text": "And another one"})
         self.assertRedirects(response, f"/lists/{our_list.id}/")
-
-
-class ListAndTaskModelTest(TestCase):
-    def test_save_load_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Task()
-        first_item.text = "First one"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Task()
-        second_item.text = "The second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(list_, saved_list)
-
-        saved_items = Task.objects.all()
-        self.assertEqual(len(saved_items), 2)
-
-        self.assertEqual(saved_items[0].text, "First one")
-        self.assertEqual(saved_items[0].list, list_)
-        self.assertEqual(saved_items[1].text, "The second")
-        self.assertEqual(saved_items[1].list, list_)
