aefdce39f4299a41cdc968a96a49bd953023fa9b
dima <dimich3d@ya.ru>
2020-04-19 14:40:16 +0300

    add fabric file

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,50 @@
+import string
+from random import choices
+
+from fabric.api import env, run, cd, local
+from fabric.contrib.files import exists, append
+
+REPO_URL = "https://github.com/festeh/mytodolist"
+
+
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+
+def _update_virtualenv():
+    if not exists('virtualenv/bin/pip'):
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt')
+
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        secret_key = ''.join(choices(string.ascii_letters, k=50))
+        append('.env', f'DJANGO_SECRET_KEY={secret_key}')
+
+
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
+
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
+
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
