bf43175b547f7ee7bcc01117b10e4fd6321c1862
dima <dimich3d@ya.ru>
2020-04-21 22:18:13 +0300

    new FT

--- functional_tests/test_list_validation.py
+++ functional_tests/test_list_validation.py
@@ -4,6 +4,8 @@ from functional_tests.base import FunctionalTest
 
 
 class TaskValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_task(self):
         self.browser.get(self.live_server_url)
@@ -35,5 +37,19 @@ class TaskValidationTest(FunctionalTest):
         # I'm trying to add a same task
         self.get_input_box_id().send_keys("Get some stuff done")
         self.get_input_box_id().send_keys(Keys.ENTER)
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
+        self.wait_for(lambda: self.assertEqual(self.get_error_element().text,
                                                "Cannot add a duplicating task"))
+
+    def test_duplication_message_are_not_shown_after_edit(self):
+        self.browser.get(self.live_server_url)
+        # I'm adding a task
+        self.get_input_box_id().send_keys("Get some stuff done")
+        self.get_input_box_id().send_keys(Keys.ENTER)
+        self.wait_for_row_task_table("1: Get some stuff done")
+        # I'm trying to add a same task
+        self.get_input_box_id().send_keys("Get some stuff done")
+        self.get_input_box_id().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertTrue(self.get_error_element().is_displayed()))
+        # I'm then starting to make an edit
+        self.get_input_box_id().send_keys("!")
+        self.wait_for(lambda: self.assertFalse(self.get_error_element().is_displayed()))
