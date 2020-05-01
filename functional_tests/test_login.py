db8f3b0465f7f73a28613aa56b4de9306de638d1
dima <dimich3d@ya.ru>
2020-05-01 21:27:55 +0300

    fix FT

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.keys import Keys
 from functional_tests.base import FunctionalTest
 
 TEST_EMAIL = "dimich3d@ya.ru"
-SUBJECT = "Your login link for TODO app"
-TEXT = "use this link to login"
+SUBJECT = "Your login link for"
+TEXT = "Use this link to login"
 
 
 class LoginTest(FunctionalTest):
@@ -24,17 +24,17 @@ class LoginTest(FunctionalTest):
         # there's new email in inbox
         email = mail.outbox[0]
         self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        self.assertIn(SUBJECT, email.subject)
         self.assertIn(TEXT, email.body)
         url_search = re.search(r"http://.+/.+$", email.body)
         if not url_search:
-            self.fail(f'Could not find email in {email.body}')
+            self.fail(f'Could not find login url in {email.body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
         # I'm clicking it
         self.browser.get(url)
 
-        self.wait_for(lambda : self.browser.find_element_by_link_text("Log out"))
+        self.wait_for(lambda: self.browser.find_element_by_link_text("Log out"))
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn(TEST_EMAIL, navbar.text)
