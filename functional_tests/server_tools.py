60c0a8d667ab8fba6b8ea7db52c79fab75ef312e
dima <dimich3d@ya.ru>
2020-05-03 12:50:02 +0300

    typo

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -4,9 +4,9 @@ from fabric.operations import run
 
 def create_server_pre_auth_server(host, email):
     manage_command = f"~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py"
-    env_lines = run(f"cat ~/sites/{host}/.env").splitlines()
-    env = dict(l.split("=") for l in env_lines if l)
     with settings(host_string=f'dima@{host}'):
+        env_lines = run(f"cat ~/sites/{host}/.env").splitlines()
+        env = dict(l.split("=") for l in env_lines if l)
         with shell_env(**env):
             session_key = run(f"{manage_command} create_session {email}")
             return session_key.strip()
