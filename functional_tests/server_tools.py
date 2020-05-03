bf0a69800672681460e7161f848c269b6b0cd60e
dima <dimich3d@ya.ru>
2020-05-03 12:43:15 +0300

    add user

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -3,16 +3,16 @@ from fabric.operations import run
 
 
 def create_server_pre_auth_server(host, email):
-    manage_command = f"~/sites/{host}/virutalenv/bin/python/ ~/sites/{host}/manage.py"
+    manage_command = f"~/sites/{host}/virtualenv/bin/python/ ~/sites/{host}/manage.py"
     env_lines = run(f"cat ~/sites/{host}/.env").splitlines()
     env = dict(l.split("=") for l in env_lines if l)
-    with settings(host_string=f'dima@deploy'):
+    with settings(host_string=f'dima@{host}'):
         with shell_env(**env):
             session_key = run(f"{manage_command} create_session {email}")
             return session_key.strip()
 
 
 def reset_database(host):
-    manage_command = f"~/sites/{host}/virutalenv/bin/python/ ~/sites/{host}/manage.py"
-    with settings(host_string=f'dima@deploy'):
+    manage_command = f"~/sites/{host}/virtualenv/bin/python/ ~/sites/{host}/manage.py"
+    with settings(host_string=f'dima@{host}'):
         run(manage_command)
