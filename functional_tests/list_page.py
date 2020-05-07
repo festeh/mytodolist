d01e5d19ef219dc6202a62e979bbcba048eeae20
dima <dimich3d@ya.ru>
2020-05-07 20:01:57 +0300

    update ft

---
+++ functional_tests/list_page.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from functional_tests.base import wait, FunctionalTest
+
+
+class ListPage:
+    def __init__(self, test):
+        self.test: FunctionalTest = test
+        self.browser: webdriver.Chrome = self.test.browser
+
+    def get_table_rows(self):
+        return self.browser.find_elements_by_css_selector("#id_task_table tr")
+
+    @wait
+    def wait_row_table(self, num, text):
+        expected = f"{num}: {text}"
+        rows = self.get_table_rows()
+        self.test.assertIn(expected, [row.text for row in rows])
+
+    def get_task_input_box(self):
+        return self.browser.find_element_by_id("id_text")
+
+    def add_task_to_list(self, text):
+        n_rows = len(self.get_table_rows())
+        self.get_task_input_box().send_keys(text)
+        self.get_task_input_box().send_keys(Keys.ENTER)
+        task_num = n_rows + 1
+        self.wait_row_table(task_num, text)
+        return self
+
+    def get_share_box(self):
+        return self.browser.find_element_by_css_selector("input[name='share]")
+
+    def get_shared_with_list(self):
+        return self.browser.find_element_by_css_selector(".list-share")
+
+    def share_task_list_with(self, email):
+        self.get_share_box().send_keys(email)
+        self.get_share_box().send_keys(Keys.ENTER)
+        self.test.wait_for(
+            lambda: self.test.assertIn(
+                email,
+                [task.text for task in self.get_shared_with_list()]
+            )
+        )
+
+    def go_to_my_lists_page(self):
+        self.browser.get(self.test.live_server_url)
+        self.browser.find_element_by_link_text("My Task Lists").click()
+        self.test.wait_for(
+            lambda: self.test.assertEqual(
+                self.test.browser.find_element_by_tag_name("h1").text,
+                "My Task Lists"
+            )
+        )
+        return self
+
+    def get_list_owner(self):
+        return self.browser.find_element_by_id("id_list_owner").text
--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,4 +1,5 @@
 from functional_tests.base import FunctionalTest
+from functional_tests.list_page import ListPage
 
 
 def force_quit(browser):
@@ -21,13 +22,25 @@ class SharingTest(FunctionalTest):
 
         # first user creates a task
         kabanchik_browser.get(self.live_server_url)
-        self.add_task_to_list("Tosi bosi")
+        kabanchik_page = ListPage(self).add_task_to_list("Tosi bosi")
 
-        share_box = self.browser.find_element_by_css_selector(
-            'input[name="share"]'
-        )
+        share_box = kabanchik_page.get_share_box()
         self.assertEqual(share_box.get_attribute("placeholder"),
                          "your-friend@example.com")
 
+        # and shares with second
+        kabanchik_page.share_task_list_with("baklanch@ik.ru")
+
+        self.browser = baklanchik_browser
+        baklanchik_page = ListPage(self).go_to_my_lists_page()
+        self.browser.find_element_by_link_text("Tosi bosi").click()
+
+        # thee verifies owner and now adds a task
+        self.assertEqual(baklanchik_page.get_list_owner(), "kabanch@ik.ru")
+        baklanchik_page.add_task_to_list("Ponyal prinyal")
+
+        self.browser = kabanchik_browser
+        self.browser.refresh()
+        kabanchik_page.wait_row_table(2, "Ponyal prinyal")
 
 
